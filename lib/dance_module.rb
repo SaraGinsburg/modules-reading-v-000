dc949a6ac34adcaada91f806f6cc9c340223fe90
Sara Ginsburg <sara.ginsburg.ny@gmail.com>
2018-04-12 03:14:24 +0000

    automatically backed up by learn

---
+++ lib/dance_module.rb
@@ -0,0 +1,28 @@
+
+CODE ALONG I: INCLUDING MODULE METHODS AS INSTANCE METHODS
+This is a code along exercise. Fork and clone this repo by clicking the Github link at the top of the page. Follow along with the walk-through below to get your code working. Get the tests to pass.
+
+STEP I: DEFINING OUR MODULE
+We'll code our Dance module inside the lib/dance_module.rb file. Open up that file and define your module with the following code:
+
+module Dance
+end
+Let's give our Dance module some fabulous moves:
+
+module Dance
+  def twirl
+    "I'm twirling!"
+  end
+
+  def jump
+    "Look how high I'm jumping!"
+  end
+
+  def pirouette
+    "I'm doing a pirouette"
+  end
+
+  def take_a_bow
+    "Thank you, thank you. It was a pleasure to dance for you all."
+  end
+end
